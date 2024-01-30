550e4d1e20ab276ab01cd45a7297f1647e32b99d
Álvaro Gutiérrez García <alvarogutierrezg99@gmail.com>
2024-01-30 19:38:20 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# ROS2_cam_video_example
+ Just a simple publisher to publish your video cam 
---
+++ build/cam_video_example/CMakeFiles/cam_video_example_uninstall.dir/compiler_depend.ts
@@ -0,0 +1,2 @@
+# CMAKE generated file: DO NOT EDIT!
+# Timestamp file for custom commands dependencies management for cam_video_example_uninstall.
